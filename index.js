d34c881250f89524f0c44530aa656db203ffcaa3
Shreyash Sanjay <shreyash6@hotmail.co.uk>
2023-04-17 14:57:16 +0100

    Improved JS code and added animations and button

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-//First Dice
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]; //Put all imgs in array
 
-document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+var die = document.querySelectorAll("img");
 
-//Second Dice
-var randomNumber2 = Math.floor(Math.random()*6)+1;
-
-document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
-
-//Winning Statement
-if (randomNumber1 > randomNumber2){
-    document.getElementById("heading").textContent = "🚩Player 1 Wins!";
-} else if (randomNumber2 > randomNumber1) {
-    document.getElementById("heading").textContent = "Player 2 Wins!🚩";
-} else {
-    document.getElementById("heading").textContent = "Draw!";
+function roll() {
+    die.forEach(function(dice) {
+        dice.classList.add("shake");
+    });
+    setTimeout(function() {
+        die.forEach(function(dice){
+            dice.classList.remove("shake");
+        });
+        var randomNumber1 = Math.floor(Math.random()*6);
+        var randomNumber2 = Math.floor(Math.random()*6);
+        document.querySelector(".img1").setAttribute("src", images[randomNumber1]);
+        document.querySelector(".img2").setAttribute("src", images[randomNumber2]);
+        if (randomNumber1 > randomNumber2){
+            document.getElementById("heading").textContent = "Player 1 Wins!";
+        } else if (randomNumber2 > randomNumber1) {
+            document.getElementById("heading").textContent = "Player 2 Wins!";
+        } else {
+            document.getElementById("heading").textContent = "Draw!";
+        }
+    }, 1000);
 }
